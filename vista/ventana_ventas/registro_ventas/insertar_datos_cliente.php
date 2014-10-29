f0561ebcbaf6c8bc6d7c09db93d9802bff20ed5d
jmayuriv <jose_16_24_4@hotmail.com>
2014-10-29 16:14:01 -0500

    insertar_datos_cliente.php
    llamado al procedimiento almacenado para porder insertar los datos de un cliente

--- vista/ventana_ventas/registro_ventas/insertar_datos_cliente.php
+++ vista/ventana_ventas/registro_ventas/insertar_datos_cliente.php
@@ -1,18 +1,24 @@
 <?php
-/*Este metodo debe ser cambiado para guardar en las tablas guia detalle, producto detalle*/
+require_once("conexion.php");
 
-/*
-$descuento_produto = 1.2;
-*/
-$tip_per=$_POST['tip_per'];
-$rsocial=$_POST['rsoc'];
-$ruc=$_POST['ruc'];
-$direccion=$_POST['direccion'];
-$usuario=$_POST['usuario'];  
+$_tip_per=$_POST['tip_per'];
+$_rsoc=$_POST['rsoc'];
+$_ruc=$_POST['ruc'];
+$_direccion=$_POST['direccion'];
+$_usuario=$_POST['usuario'];
+$fecha_hora_actual =Fechas::mifechagmt(time(),-5);
 
-echo $usuario;
+$query_call_spcabped = "CALL proc_ingresar_cliente('".$_rsoc."',".$_tip_per.",'"
+	                                                .$_direccion."','".$_ruc."','"
+	                                                .$_usuario."','".$fecha_hora_actual."',@n_Flag, @c_msg, @n_cod)";
 
+$array_flag = mysql_fetch_array(mysql_query("Select @n_Flag",Conectar::con()));
+$array_codgen = mysql_fetch_array(mysql_query("Select @n_cod",Conectar::con()));
+$array_msg = mysql_fetch_array(mysql_query("Select @c_msg",Conectar::con()));
 
-
+$codigo_flag = $array_flag["@n_Flag"];
+$codigo_gen = $array_codgen["@n_cod"]; 
+$codi_msg = $array_codgen["@c_msg"]; 
+echo $codi_msg;
 
 ?>
