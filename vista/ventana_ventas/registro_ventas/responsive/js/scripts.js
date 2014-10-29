57a45209e362d9f2012afb5f93d20596d2a4943b
sbcc-peru <jcutti@sbcc-peru.com>
2014-10-29 14:29:20 -0500

    validaciones en basicas
    incluyo validaciones de menu de tipo de documentos

--- vista/ventana_ventas/registro_ventas/responsive/js/scripts.js
+++ vista/ventana_ventas/registro_ventas/responsive/js/scripts.js
@@ -38,3 +38,53 @@ www.toddmotto.com
 	}
 
 })();
+
+
+
+function Solo_Numerico(variable){
+    Numer=parseInt(variable);
+    if (isNaN(Numer)){
+       return "";
+       }
+       return Numer;
+    }
+    function ValNumero(Control){
+      Control.value=Solo_Numerico(Control.value);
+    }
+	
+	
+	function tab(ev,obj) {
+var keyCode = document.layers ? ev.which : document.all ? event.keyCode : document.getElementById ? ev.keyCode : 0;
+if (keyCode !=13) return;
+frm = obj.form;
+for(i = 0; i < frm.elements.length; i++) 
+if (frm.elements[i] == obj) { 
+if (i == frm.elements.length-1) i =-1;
+break 
+}
+frm.elements[i+1].focus();
+return false;
+}
+function decimal(evt) {
+var keyPressed = (evt.which) ? evt.which : event.keyCode
+return !((keyPressed !=13) && (keyPressed != 46) && (keyPressed < 48 || keyPressed > 57));
+
+
+
+}
+
+function validar_tipo_doc(){
+	
+		var factura = document.getElementById("tip_per").value;
+	
+		if (factura==2)
+		{
+			document.getElementById("rsocial").innerHTML = "R. SOCIAL :";
+			document.getElementById("ruc").innerHTML = "RUC :";
+		}
+		else
+		{
+			document.getElementById("rsocial").innerHTML = "NOMBRES:";
+			document.getElementById("ruc").innerHTML = "DNI:";
+		}
+	}
