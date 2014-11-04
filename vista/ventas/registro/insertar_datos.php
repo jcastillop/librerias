0a0a8ec8118022b5ce1fa8bd0dd1c49bcb105bdf
jmayuriv <jose_16_24_4@hotmail.com>
2014-11-04 12:09:25 -0500

    Modifiacion series Venta

--- vista/ventas/registro/insertar_datos.php
+++ vista/ventas/registro/insertar_datos.php
@@ -33,34 +33,37 @@ $codigo_flag = $array_flag["@n_Flag"];
 $codigo_gen = $array_codgen["@cod_generado"]; 
 
 //creando query del PA insertar guia cabecera
-$query_call_spcabgui = "CALL proc_insertar_guia_cab(".$_cod_suc.",".$_cod_emp.",'".$_cod_ser."','',"
+$query_call_spcabgui = "CALL proc_insertar_guia_cab(".$_cod_suc.",".$_cod_emp.",'',"
                                                                        .$_cod_cli.",'".$codigo_gen."', 2, 1,'','',"
                                                                        .$_cod_usu.",'',0,'','','','','','','','"
-                                                                       .$_fec_pedido."','".$_ped_usu."',@n_Flag1, @c_msg1, @cod_generado1)";
+                                                                       .$_fec_pedido."','".$_ped_usu."',@n_Flag1, @c_msg1, @cod_generado1, @c_serie)";
 
 mysql_query($query_call_spcabgui,Conectar::con());
 
 $codigo_msg1 = "";
 $array_flag1 = mysql_fetch_array(mysql_query("Select @n_Flag1",Conectar::con()));
 $array_codgen1 = mysql_fetch_array(mysql_query("Select @cod_generado1",Conectar::con()));
+$array_codsergui = mysql_fetch_array(mysql_query("Select @c_serie",Conectar::con()));
 $codigo_flag1 = $array_flag1["@n_Flag1"];
 $codigo_gen1 = $array_codgen1["@cod_generado1"]; 
+$cod_sergui = $array_codsergui["@c_serie"];
 
 
 //creando query del PA insertar Factura cabecera
-$query_call_spfact = "CALL proc_insertar_fact_cab(".$_cod_suc.",".$_cod_emp.",'".$_cod_ser."',"
-                                                                       .$_cod_cli.",'".$codigo_gen1."',"
+$query_call_spfact = "CALL proc_insertar_fact_cab(".$_cod_suc.",".$_cod_emp.",".$_cod_cli.",'".$codigo_gen1."',"
                                                                        .$_cod_usu.",'"
-                                                                       .$_fec_pedido."','".$_tipo_doc."','".$_ped_usu."',@n_Flag3, @c_msg3, @cod_generado3)";
+                                                                       .$_fec_pedido."',".$_tipo_doc.",'".$_ped_usu.
+                                                                       "',@n_Flag3, @c_msg3, @cod_generado3,@cod_ser)";
 
 mysql_query($query_call_spfact,Conectar::con());
 
 $codigo_msg3 = "";
 $array_flag3 = mysql_fetch_array(mysql_query("Select @n_Flag3",Conectar::con()));
 $array_codgen3 = mysql_fetch_array(mysql_query("Select @cod_generado3",Conectar::con()));
+$array_codser = mysql_fetch_array(mysql_query("Select @cod_ser",Conectar::con()));
 $codigo_flag3 = $array_flag3["@n_Flag3"];
-$codigo_gen3 = $array_codgen3["@cod_generado3"]; 
-
+$codigo_gen3 = $array_codgen3["@cod_generado3"];
+$_cod_ser = $array_codser["@cod_ser"];
 
 if ($codigo_flag==0) {
    $var_ped_detalle="'";
@@ -68,11 +71,11 @@ if ($codigo_flag==0) {
        $var_cod_ped_det=$i+1;
 	   $codigo_libro=$array[$i]->codigo_libro;
        $cantidad_libro = $array[$i]->cantidad_libro;
-       $valor_impuesto = $array[$i]->valor_impuesto;
-       $valor_descuento = $array[$i]->valor_descuento;
-       $porcentaje_impuesto = $array[$i]->porcentaje_impuesto;
-       $porcentaje_descuento = $array[$i]->porcentaje_descuento;
-       $costo_total_libro = $array[$i]->costo_total_libro;
+       $valor_impuesto = number_format($array[$i]->valor_impuesto, 2, '.', '');
+       $valor_descuento = number_format($array[$i]->valor_descuento, 2, '.', '');
+       $porcentaje_impuesto = number_format($array[$i]->porcentaje_impuesto, 2, '.', '');
+       $porcentaje_descuento = number_format($array[$i]->porcentaje_descuento, 2, '.', '');
+       $costo_total_libro = number_format($array[$i]->costo_total_libro, 2, '.', '');
 $var_ped_detalle=$var_ped_detalle.'(lpad("'.$var_cod_ped_det.'",6,"0"),'
        										  .'"'.$codigo_gen.'"'.", ".$_cod_suc.", ".$_cod_emp.", ". 
        	                                       $codigo_libro.", ".$cantidad_libro.", ".$porcentaje_impuesto.", ".$valor_impuesto. ", ".
@@ -100,16 +103,18 @@ if ($codigo_flag1==0) {
        $var_cod_guia_det=$i+1;
 	   $codigo_libro=$array[$i]->codigo_libro;
        $cantidad_libro = $array[$i]->cantidad_libro;
-       $valor_impuesto = $array[$i]->valor_impuesto;
-       $valor_descuento = $array[$i]->valor_descuento;
-       $porcentaje_impuesto = $array[$i]->porcentaje_impuesto;
-       $porcentaje_descuento = $array[$i]->porcentaje_descuento;
-       $costo_total_libro = $array[$i]->costo_total_libro;
+       $valor_pvent = 0.00;
+       $valor_impuesto = number_format($array[$i]->valor_impuesto, 2, '.', '');
+       $valor_descuento = number_format($array[$i]->valor_descuento, 2, '.', '');
+       $porcentaje_impuesto = number_format($array[$i]->porcentaje_impuesto, 2, '.', '');
+       $porcentaje_descuento = number_format($array[$i]->porcentaje_descuento, 2, '.', '');
+       $costo_total_libro = number_format($array[$i]->costo_total_libro, 2, '.', '');
+       $costo_final_libro = number_format(($array[$i]->costo_total_libro+$array[$i]->valor_impuesto-$array[$i]->valor_descuento), 2, '.', '');
 $var_guia_detalle=$var_guia_detalle.'(lpad("'.$var_cod_guia_det.'",6,"0"),'
-       										  .'"'.$codigo_gen1.'"'.", ".$_cod_ser.", ".$_cod_suc.", ".$_cod_emp.", ". 
-       	                                       $codigo_libro.", ".$cantidad_libro.", ".$porcentaje_impuesto.", ".$valor_impuesto. ", ".
-       	                                       $costo_total_libro. ", ".$porcentaje_descuento.",".$valor_descuento.", ".$costo_total_libro.
-       	                                       ',"'.$_ped_usu.'","'.$fecha_hora_actual.'")';
+       										  .'"'.$codigo_gen1.'"'.", ".'"'.$cod_sergui.'"'.", ".$_cod_suc.", ".
+                                               $_cod_emp.", ". $codigo_libro.", ".$cantidad_libro.",".$valor_pvent.", ".
+                                               $costo_total_libro. ", ".$porcentaje_impuesto.", ".$valor_impuesto. ", ".
+                                               $porcentaje_descuento.",".$valor_descuento.", ".$costo_final_libro.',"'.$_ped_usu.'","'.$fecha_hora_actual.'")';
 
        if ($i==count($array)-1){
           $var_guia_detalle = $var_guia_detalle . "'";
@@ -129,15 +134,16 @@ $var_guia_detalle=$var_guia_detalle.'(lpad("'.$var_cod_guia_det.'",6,"0"),'
    $var_fact_detalle="'";
    for($i=0;$i<count($array);$i++){ 
        $var_cod_fact_det=$i+1;
+       $int_tip_doc_fact=1;
 	   $codigo_libro=$array[$i]->codigo_libro;
        $cantidad_libro = $array[$i]->cantidad_libro;
-       $valor_impuesto = $array[$i]->valor_impuesto;
-       $valor_descuento = $array[$i]->valor_descuento;
-       $porcentaje_impuesto = $array[$i]->porcentaje_impuesto;
-       $porcentaje_descuento = $array[$i]->porcentaje_descuento;
-       $costo_total_libro = $array[$i]->costo_total_libro;
+       $valor_impuesto = number_format($array[$i]->valor_impuesto, 2, '.', '');
+       $valor_descuento = number_format($array[$i]->valor_descuento, 2, '.', '');
+       $porcentaje_impuesto = number_format($array[$i]->porcentaje_impuesto, 2, '.', '');
+       $porcentaje_descuento = number_format($array[$i]->porcentaje_descuento, 2, '.', '');
+       $costo_total_libro = number_format($array[$i]->costo_total_libro, 2, '.', '');
 $var_fact_detalle=$var_fact_detalle.'(lpad("'.$var_cod_fact_det.'",6,"0"),'
-       										  .'"'.$codigo_gen3.'"'.", ".$_cod_ser.", ".$_cod_suc.", ".$_cod_emp.", ". 
+       										  .'"'.$codigo_gen3.'"'.", ".$int_tip_doc_fact.",".'"'.$_cod_ser.'"'.", ".$_cod_suc.", ".$_cod_emp.", ". 
        	                                       $codigo_libro.", ".$cantidad_libro.", ".$porcentaje_impuesto.", ".$valor_impuesto. ", ".
        	                                       $costo_total_libro. ", ".$porcentaje_descuento.",".$valor_descuento.", ".$costo_total_libro.
        	                                       ',"'.$_ped_usu.'","'.$fecha_hora_actual.'")';
@@ -159,4 +165,3 @@ $var_fact_detalle=$var_fact_detalle.'(lpad("'.$var_cod_fact_det.'",6,"0"),'
    
 echo $codigo_msg1;
 ?> 
-
