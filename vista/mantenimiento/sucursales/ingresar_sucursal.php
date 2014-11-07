09181d1882a57a81b1e052b1b86cec0069c3059c
silvasaman <silvasamanj@gmail.com>
2014-11-07 03:41:40 -0500

    cambi

--- vista/mantenimiento/sucursales/ingresar_sucursal.php
+++ vista/mantenimiento/sucursales/ingresar_sucursal.php
@@ -6,5 +6,17 @@ require_once("../../../conexiones/class_sucursal.php");
 require_once("../../../conexiones/conexion.php");
 //print_r($_POST);
 $tra=new sucursal();
-$tra->add_sucursal($_POST["cod_emp"],$_POST["var_nom_suc"],$_POST["descripcion"],$_POST["estado"],$_POST["pais"],$_POST["departamento"],$_POST["provincia"],$_POST["direccion"],$_POST["telf"],$user,$fecha_actual,$user,$fecha_actual);
+if($_POST['pais']==999)
+{
+	$pais=184;
+	$departamento=15;
+	$provincia=128;
+}
+else
+{
+	$pais= $_POST['pais'];
+	$departamento= $_POST['departamento'];
+	$provincia= $_POST['provincia'];
+}
+$tra->add_sucursal($_POST["cod_emp"],$_POST["var_nom_suc"],$_POST["descripcion"],$_POST["estado"],$pais,$departamento,$provincia,$_POST["direccion"],$_POST["telf"],$user,$fecha_actual,$user,$fecha_actual);
 ?>
