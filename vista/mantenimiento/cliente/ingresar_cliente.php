b2d24e69bf4eebe62012be6245c96b1b9b4bb745
cutti1990 <cutti1990@gmail.com>
2014-11-07 02:35:18 -0500

    validar pais datos

--- vista/mantenimiento/cliente/ingresar_cliente.php
+++ vista/mantenimiento/cliente/ingresar_cliente.php
@@ -6,15 +6,30 @@ require_once("../../../conexiones/class_cliente.php");
 require_once("../../../conexiones/conexion.php");
 //print_r($_POST);nick_usu
 $tra=new cliente();
+if($_POST['pais']==999)
+{
+	$pais=184;
+	$departamento=15;
+	$provincia=128;
+}
+else
+{
+	$pais= $_POST['pais'];
+	$departamento= $_POST['departamento'];
+	$provincia= $_POST['provincia'];
+}
+
+					 
 $tra->add_cliente($_POST['rsoc'],
 				 $_POST['estado'],
+				 $ide_suc,
 				 $_POST['tip_per'],
 				 $_POST['ruc'],
 				 $_POST['direccion'],
 				 $_POST['refdom'],
-				 $_POST['pais'],
-				 $_POST['departamento'],
-				 $_POST['provincia'],
+				 $pais,
+				 $departamento,
+				 $provincia,
 				 $_POST['distrito'],
 				 $_POST['telefono'],
 				 $_POST['fax'],
