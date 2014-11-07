7cc45d03255d262251f3210ade46802691cf34ab
silvasaman <silvasamanj@gmail.com>
2014-11-07 00:02:46 -0500

    modifico get_cliente

--- conexiones/class_cliente.php
+++ conexiones/class_cliente.php
@@ -17,23 +17,17 @@ class cliente
 				c.int_cod_cli,
 				c.var_rsoc_cli,
 				case when c.int_est_cli=1 then 'Activo' else 'Inactivo' end int_est_cli,
-				case when c.int_tipper_cli=1 then 'Natural' ELSE 'Juridico' END int_tipper_cli,
 				c.var_ruc_cli,
 				c.var_dir_cli,
 				c.var_refdom_cli,
-				c.int_cod_pais,
-				p.var_nom_pais,
 				c.int_cod_dept,
 				d.var_nom_dept,
 				c.int_cod_provi,
 				pr.var_nom_provi,
 				c.var_dist_cli,
 				c.var_telf_cli,
-				c.var_fax_cli,
-				c.var_dni_cli,
 				c.var_cor_cli
 				from T_cliente c
-				inner join T_pais p on p.int_cod_pais=c.int_cod_pais
 				inner join T_departamentos d on d.int_cod_dept=c.int_cod_dept
 				inner join T_provincias pr on pr.int_cod_provi=c.int_cod_provi
 				where int_est_cli<>0
