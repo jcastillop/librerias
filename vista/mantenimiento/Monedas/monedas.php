9c065ffcd1cd64736207aa7b0cd6bc4a0aa32781
dlandechop <dlandecho@sbcc-peru.com>
2014-11-12 10:10:56 -0500

    monedas
    este archivo permite un nuevo registro al formulario

---
+++ vista/mantenimiento/Monedas/monedas.php
@@ -0,0 +1,204 @@
+<script> 
+function cerrarse(){ 
+window.close() 
+} 
+</script> 
+<?php
+require_once("../../../conexiones/class_monedas.php");
+require_once("../../../conexiones/conexion.php");
+?>
+  
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<head>
+
+<!--------------------
+LOGIN FORM
+by: Amit Jakhu
+www.amitjakhu.com
+--------------------->
+
+<!--META-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MONEDAS</title>
+
+<!--STYLESHEETS-->
+<link href="../../../paquetes/css ventanas/style_ventana.css" rel="stylesheet" type="text/css" />
+
+<!--SCRIPTS-->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+ <script type="text/javascript" src="../../../paquetes/js/validar.js"></script>
+
+ <script type="text/javascript" src="js/validar.js"></script>
+<!--Slider-in icons-->
+<script type="text/javascript">
+$(document).ready(function() {
+	$(".username").focus(function() {
+		$(".user-icon").css("left","-48px");
+	});
+	$(".username").blur(function() {
+		$(".user-icon").css("left","0px");
+	});
+	
+	$(".password").focus(function() {
+		$(".pass-icon").css("left","-48px");
+	});
+	$(".password").blur(function() {
+		$(".pass-icon").css("left","0px");
+	});
+});
+function obtiene_http_request()
+{
+var req = false;
+try
+  {
+    req = new XMLHttpRequest(); /* p.e. Firefox */
+  }
+catch(err1)
+  {
+  try
+    {
+     req = new ActiveXObject("Msxml2.XMLHTTP");
+  /* algunas versiones IE */
+    }
+  catch(err2)
+    {
+    try
+      {
+       req = new ActiveXObject("Microsoft.XMLHTTP");
+  /* algunas versiones IE */
+      }
+      catch(err3)
+        {
+         req = false;
+        }
+    }
+  }
+return req;
+}
+var miPeticion = obtiene_http_request();
+
+
+
+function from(id,ide,url){
+		var mi_aleatorio=parseInt(Math.random()*99999999);//para que no guarde la p�gina en el cach�...
+		var vinculo=url+"?id="+id+"&rand="+mi_aleatorio;
+		//alert(vinculo);
+		miPeticion.open("GET",vinculo,true);//ponemos true para que la petici�n sea asincr�nica
+		miPeticion.onreadystatechange=miPeticion.onreadystatechange=function(){
+               if (miPeticion.readyState==4)
+               {
+				   //alert(miPeticion.readyState);
+                       if (miPeticion.status==200)
+                       {
+                                //alert(miPeticion.status);
+                               //var http=miPeticion.responseXML;
+                               var http=miPeticion.responseText;
+                               document.getElementById(ide).innerHTML= http;
+
+                       }
+               }/*else
+               {
+			document.getElementById(ide).innerHTML="<img src='ima/loading.gif' title='cargando...' />";
+
+                }*/
+       }
+       miPeticion.send(null);
+
+}
+
+function validar(e) { // 1
+
+    tecla = (document.all) ? e.keyCode : e.which; // 2
+
+    if (tecla==8) return true; // 3
+
+    patron =/[A-Za-z\s]/; // 4
+
+    te = String.fromCharCode(tecla); // 5
+
+    return patron.test(te); // 6
+
+} 
+function formulario(f) {
+	if (f.var_nom_mon.value   == '') { alert ('El campo Nombre moneda esta vacío, ingrese un dato porfavor!!');  
+	f.var_nom_mon.focus(); return false; }  
+	
+ return true; } 
+</script>
+
+</head>
+<body <?php if (isset($_GET['load'])){ echo "onload='cerrar();'";  } ?>   >
+
+<!--WRAPPER-->
+<div id="wrapper">
+
+	<!--SLIDE-IN ICONS-->
+    <div class="user-icon_"></div>
+    <div class="pass-icon_"></div>
+    <!--END SLIDE-IN ICONS-->
+    <br />
+    <br />
+
+<!--LOGIN FORM-->
+<form name="form1" class="login-form" action="ingresar_monedas.php" method="post" onSubmit="return formulario(this)">
+
+	<!--HEADER-->
+    <div class="header">
+    <!--TITLE-->
+    <h1 align="center">MONEDAS</h1>	
+    
+    <!--END TITLE-->
+  
+    </div>
+    <!--END HEADER-->
+	
+	<!--CONTENT-->
+    <div class="content">
+      <table width="80%" border="1">
+        <tr>
+          <td height="20">Nombre Monedas: </td>
+          <td><input name="var_nom_mon" type="text" maxlength="10" style="width: 400px;" class="input username" onKeyPress="return tab(event,this)" /></td>	
+           </tr>
+        <tr>
+          <td height="20">Descripcion: </td>
+        <td><input type="text" class="input username" style="width: 160px;"   name="var_des_mon" id="var_des_mon" /></td>
+		   Estado:
+          <select class="input username" style="width: 155px;" name="int_est_mon" id="int_est_mon"  >
+              <option selected='selected' value='1'>Activo</option>
+            
+           <option  value='0'>Inactivo</option>
+         
+          
+      
+      </select>
+    </tr>
+        
+       </table>
+    </div>
+    <!--END CONTENT-->
+    
+    <!--FOOTER-->
+    <div class="footer" >
+    <!--LOGIN BUTTON--><input type="submit" name="submit" value="GUARDAR" class="button" /><!--END LOGIN BUTTON-->
+    <!--REGISTER BUTTON--><input type="button" name="submit" value="CANCELAR" class="register"onClick="cerrarse()" /><!--END REGISTER BUTTON-->
+    <input type="hidden" id="val1" value="" disabled="disabled"/> 
+    </div>
+    <!--END FOOTER-->
+
+</form>
+<!--END LOGIN FORM-->
+
+</div>
+<!--END WRAPPER-->
+
+<!--GRADIENT--><!--END GRADIENT-->
+
+</body>
+</html>
